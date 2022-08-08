b56bee7372e31a2b32c30249d025a1db3e965c98
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-08-08 20:53:05 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import Profile from "./painting";
+import user from './user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        avatar={user[0].avatar}
+        username={user[0].username}
+        tag={user[0].tag}
+        location={user[0].location}
+        stats={user[0].stats}
+      />
     </div>
   );
 };
